27553a0d918b7d88352cf5519d0cf839894ec152
Lucas B <56450683+lucasbd11@users.noreply.github.com>
2021-11-06 00:08:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import nextcord
 from nextcord.ext import commands
-import sqlite3
+#import sqlite3
 import json
 
 """
 Bot discord pour avoir les disponibiltés des membres du serveur
 Réalisé par Lucas
-Version 1.2.0
+Version 1.3.3
 """
 
 #creating sql file
-conn = sqlite3.connect('membresDATA.db')
-c = conn.cursor()
+# conn = sqlite3.connect('membresDATA.db')
+# c = conn.cursor()
 
 
 #creating sql db:
-try:
-    c.execute('''CREATE TABLE membresDATA
-             (id text PRIMARY KEY,dispo text,heures int)''')
-except:
-    pass
+# try:
+#     c.execute('''CREATE TABLE membresDATA
+#              (id text PRIMARY KEY,dispo text,heures int)''')
+# except:
+#     pass
 
 
 
@@ -27,6 +27,16 @@ ADMIN_ROLES = ["Respo Job"]
 
 DATA = {}
 
+
+try:
+    file = open("DATA.txt","r")
+    dataTxt = file.read()
+    DATA = json.loads(dataTxt)
+    file.close()
+    print("ok")
+except:
+    print("pas ok")
+    pass
 
 def getnames(guild,id):
     nickName = guild.get_member(int(id)).nick
@@ -37,11 +47,11 @@ def getnames(guild,id):
         return nickName
 
 class HorairesDispo(nextcord.ui.Select):
-    def __init__(self,user):
+    def __init__(self,user,days):
 
 
         options = []
-        for day in DATA[str(user.id)]["day"]:
+        for day in days:
             for trancheHoraire in ["matin","après-midi","soir"]:
                 if trancheHoraire == "matin":
                     options.append(nextcord.SelectOption(label=day+" "+trancheHoraire, description="Tu es disponible"+ day+" "+trancheHoraire, emoji='🌅'))
@@ -53,15 +63,26 @@ class HorairesDispo(nextcord.ui.Select):
 
 
 
-        super().__init__(placeholder='Choisis tes disponibilités...', min_values=1, max_values=len(DATA[str(user.id)]["day"])*3, options=options)
+        super().__init__(placeholder='Choisis tes disponibilités...', min_values=1, max_values=len(days)*3, options=options)
 
     async def callback(self, interaction: nextcord.Interaction):
 
+        ajout = ""
+
+        if DATA[str(interaction.user.id)]["trancheHoraires"] == None:
+            DATA[str(interaction.user.id)]["trancheHoraires"] = self.values
+        else:
+
+            for tranche in self.values:
+
+                if not(tranche in DATA[str(interaction.user.id)]["trancheHoraires"]):
+                    DATA[str(interaction.user.id)]["trancheHoraires"] += [tranche]
+            ajout = " ajoutées"
 
 
-        DATA[str(interaction.user.id)]["trancheHoraires"] = self.values
 
-        dictDispo = {"Lundi":[],"Mardi":[],"Mercredi":[],"Jeudi":[],"Vendredi":[],"Dimanche":[]}
+
+        dictDispo = {"Lundi":[],"Mardi":[],"Mercredi":[],"Jeudi":[],"Vendredi":[],"Samedi":[],"Dimanche":[]}
 
 
         for i in self.values:
@@ -70,13 +91,13 @@ class HorairesDispo(nextcord.ui.Select):
 
 
         dispos = "\n".join(["- " + i + " ("+ ", ".join(dictDispo[i])+")" for i in dictDispo if len(dictDispo[i]) != 0])
-        await interaction.response.send_message(f'**Récapitulatif de tes disponibilités:**\n{dispos}\n\n_Si tu as fais une erreur tu peux séléctionner à nouveau_',ephemeral=True)
+        await interaction.response.send_message(f'**Récapitulatif de tes disponibilités{ajout}:**\n{dispos}\n\n_Si tu as fais une erreur tu peux séléctionner à nouveau_',ephemeral=True)
 
 
 class HorairesDispoView(nextcord.ui.View):
-    def __init__(self,user):
+    def __init__(self,user,days):
         super().__init__()
-        self.add_item(HorairesDispo(user))
+        self.add_item(HorairesDispo(user,days))
 
 
 
@@ -103,6 +124,8 @@ class Dispo(nextcord.ui.Select):
     async def callback(self, interaction: nextcord.Interaction):
 
 
+        ajout = ""
+
         if "Aucun" in self.values:
             DATA[str(interaction.user.id)] = {"day":["Aucun"],"trancheHoraires":None}
             listDispo = ["Aucun"]
@@ -111,12 +134,19 @@ class Dispo(nextcord.ui.Select):
 
             await interaction.response.send_message("Tu n'es pas disponible cette semaine.\n\n_Si tu as fais une erreur tu peux séléctionner à nouveau_",ephemeral=True)
         else:
-            DATA[str(interaction.user.id)] = {"day":self.values,"trancheHoraires":None}
+
+            if str(interaction.user.id) in DATA:
+                for day in self.values:
+                    if not(day in DATA[str(interaction.user.id)]["day"]):
+                        DATA[str(interaction.user.id)]["day"] += [day]
+                ajout = " ajoutées"
+            else:
+                DATA[str(interaction.user.id)] = {"day":self.values,"trancheHoraires":None}
 
 
 
             dispos = "\n".join(["- " + i for i in self.values])
-            await interaction.response.send_message(f'**Récapitulatif de tes disponibilités:**\n{dispos}\n\n_Si tu as fais une erreur tu peux séléctionner à nouveau_\n\nSélectionne maintenant les moments dans la journée où tu es disponible',view= HorairesDispoView(interaction.user), ephemeral=True)
+            await interaction.response.send_message(f'**Récapitulatif de tes disponibilités{ajout}:**\n{dispos}\n\n_Si tu as fais une erreur tu peux séléctionner à nouveau_\n\nSélectionne maintenant les moments dans la journée où tu es disponible',view= HorairesDispoView(interaction.user,self.values), ephemeral=True)
             #listDispo = self.values
 
         # try:
@@ -163,7 +193,8 @@ class Check(nextcord.ui.Select):
 
 
 
-                dictDispo = {"Lundi":[],"Mardi":[],"Mercredi":[],"Jeudi":[],"Vendredi":[],"Dimanche":[]}
+                dictDispo = {"Lundi":[],"Mardi":[],"Mercredi":[],"Jeudi":[],"Vendredi":[],"Samedi":[],"Dimanche":[]}
+
 
                 for i in DATA[nickToID[self.values[0]]]["trancheHoraires"]:
                     dictDispo[i[:i.find(" ")]].append(i[i.find(" ")+1:])
@@ -242,6 +273,102 @@ bot = commands.Bot(command_prefix = "!",intents=intents)
 
 
 
+
+
+class RemoveDispoSlot(nextcord.ui.Select):
+    def __init__(self,user,values):
+
+
+
+        listdispo = [tranche for tranche in DATA[str(user.id)]["trancheHoraires"] if tranche[:tranche.find(" ")] in values]
+
+        options = [nextcord.SelectOption(label=id, description='Clique pour supprimer cette disponibilité', emoji='❌') for id in listdispo]
+
+
+        super().__init__(placeholder='Choisis les jours...', min_values=1, max_values=len(listdispo), options=options)
+
+    async def callback(self, interaction: nextcord.Interaction):
+
+            removedSlots = []
+            toRemove = []
+            for slot in self.values:
+                if slot in DATA[str(interaction.user.id)]["trancheHoraires"]:
+                    DATA[str(interaction.user.id)]["trancheHoraires"].remove(slot)
+                    removedSlots.append(slot)
+
+            print(DATA[str(interaction.user.id)])
+            for day in DATA[str(interaction.user.id)]["day"]:
+                check = True
+
+                for i in DATA[str(interaction.user.id)]["trancheHoraires"]:
+                    if i.find(day) > -1:
+                        check = False
+                print(day,check)
+                if check:
+                    toRemove += [day]
+
+            for i in toRemove:
+                DATA[str(interaction.user.id)]["day"].remove(i)
+
+
+
+            dispos = "\n".join(["- " + i for i in removedSlots])
+            await interaction.response.send_message(f'**Récapitulatif des créneaux supprimés:**\n{dispos}\n\n_Si tu as fais une erreur tu peux séléctionner à nouveau_', ephemeral=True)
+
+
+
+
+class RemoveDispoSlotView(nextcord.ui.View):
+    def __init__(self,user,values):
+        super().__init__()
+        self.add_item(RemoveDispoSlot(user,values))
+
+
+
+
+class RemoveDispoDay(nextcord.ui.Select):
+    def __init__(self):
+
+        options = [
+            nextcord.SelectOption(label='Lundi', description='Tu es disponible lundi', emoji='❌'),
+            nextcord.SelectOption(label='Mardi', description='Tu es disponible mardi', emoji='❌'),
+            nextcord.SelectOption(label='Mercredi', description='Tu es disponible mercredi', emoji='❌'),
+            nextcord.SelectOption(label='Jeudi', description='Tu es disponible jeudi', emoji='❌'),
+            nextcord.SelectOption(label='Vendredi', description='Tu es disponible vendredi', emoji='❌'),
+            nextcord.SelectOption(label='Samedi', description='Tu es disponible samedi', emoji='❌'),
+            nextcord.SelectOption(label='Dimanche', description='Tu es disponible dimanche', emoji='❌')
+        ]
+
+
+        super().__init__(placeholder='Choisis les jours...', min_values=1, max_values=7, options=options)
+
+    async def callback(self, interaction: nextcord.Interaction):
+
+            removedDays = []
+
+            for day in self.values:
+                if day in DATA[str(interaction.user.id)]["day"]:
+                    removedDays.append(day)
+
+
+
+            dispos = "\n".join(["- " + i for i in removedDays])
+
+            if len(removedDays) == 0:
+                await interaction.response.send_message('Aucune disponibilité trouvée pour ce/ces jour(s)', ephemeral=True)
+            else:
+                await interaction.response.send_message(f'**Récapitulatif des jours sélectionnés:**\n{dispos}\n\n_Si tu as fais une erreur tu peux séléctionner à nouveau_\n\nSélectionne maintenant les moments dans la journée où tu veux supprimer ta disponibilité',view= RemoveDispoSlotView(interaction.user,self.values), ephemeral=True)
+
+
+
+
+class RemoveDispoDayView(nextcord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.add_item(RemoveDispoDay())
+
+
+
 @bot.command(aliases = ["cd"])
 async def checkDay(ctx):
     if max([role in [i.name for i in ctx.message.author.roles] for role in ADMIN_ROLES]):
@@ -268,10 +395,53 @@ async def checkUser(ctx):
 @bot.command(aliases = ["it"])
 async def interim(ctx):
 
+    #if max([role in [i.name for i in ctx.message.author.roles] for role in ADMIN_ROLES]):
+    view = DispoView()
+    #await ctx.message.delete()
+    await ctx.send("Sélectionne ci dessous la liste des jours, où tu es disponible cette semaine pour faire de l'intérim (si tu n'es pas disponible de la semaine clique sur 'aucun'", view=view)
+
+@bot.command(aliases = ["itr"])
+async def interimremove(ctx):
+
+    #if max([role in [i.name for i in ctx.message.author.roles] for role in ADMIN_ROLES]):
+    view = RemoveDispoDayView()
+    #await ctx.message.delete()
+    await ctx.send("Sélectionne ci dessous les jours dont tu veux supprimer des disponibilités (pour supprimer totalement un jour supprime tous les créneaux horaires de celui ci", view=view)
+
+
+
+@bot.command(aliases = ["md"])
+async def mesdispos(ctx,*param):
+    message = ctx.message
+    userDM = await message.author.create_dm()
+
+    dictDispo = {"Lundi":[],"Mardi":[],"Mercredi":[],"Jeudi":[],"Vendredi":[],"Samedi":[],"Dimanche":[]}
+
+
+    for i in DATA[str(ctx.author.id)]["trancheHoraires"]:
+        dictDispo[i[:i.find(" ")]].append(i[i.find(" ")+1:])
+
+    dispos = "\n".join(["- " + i + " ("+ ", ".join(dictDispo[i])+")" for i in dictDispo if len(dictDispo[i]) != 0])
+
+
+
+    await userDM.send(f"Voici la liste de tes disponibilités:\n{dispos}")
+
+
+
+
+@bot.command()
+async def data(ctx):
+    message = ctx.message
     if max([role in [i.name for i in ctx.message.author.roles] for role in ADMIN_ROLES]):
-        view = DispoView()
-        await ctx.message.delete()
-        await ctx.send("Sélectionne ci dessous la liste des jours, où tu es disponible cette semaine pour faire de l'intérim (si tu n'es pas disponible de la semaine clique sur 'aucun'", view=view)
+        file = open("DataDispo.txt","w+")
+        jsonData = json.dumps(DATA)
+
+        file.write(jsonData)
+        file.close()
+        await message.channel.send(file=nextcord.File("DataDispo.txt"))
+
+
 
 
 bot.run("TOKEN")
